df2d7c3dbd792b58dd7b7410e5545218c00ac770
Tim Graham <timothy.graham3@uq.net.au>
2016-05-11 18:14:51 +1000

    Mostly undo the changes from previous build that didnt work

--- SocialMediaLab/R/searchCommentsForMentions.R
+++ SocialMediaLab/R/searchCommentsForMentions.R
@@ -21,9 +21,9 @@ function (commentsTextCleaned,usernamesCleaned) {
     }
   )
 
-  # matchTemp <- unlist(matchTemp)
+  matchTemp <- unlist(matchTemp)
   # matchTemp <- as.vector(matchTemp)
-  matchTemp <- iconv(matchTemp, to = 'UTF-8')
+  # matchTemp <- iconv(matchTemp, to = 'UTF-8')
 
   # have to split `matchTemp` into as many groups as there are rows (i.e. comment texts)
   matchTemp2 <- split(matchTemp, ceiling(seq_along(matchTemp)/length(commentsTextCleaned)))
@@ -37,27 +37,31 @@ function (commentsTextCleaned,usernamesCleaned) {
   # So, we want to ensure it takes the username with more characters (i.e. "Timothy"),
   # rather than the subset match (i.e. "Tim").
 
-  # matchTemp3 <- lapply(matchTemp2, function(x) {
-  #
-  #   # if all elements == "FALSE" then just return "FALSE"
-  #     if (length(x[which(x=="FALSE")])==length(x)) {
-  #       return("FALSE")
-  #     }
-  #
-  #   # if all elements except one == "FALSE" then return the 'non false' element
-  #   # e.g. c("FALSE", "FALSE", "Timothy", "FALSE") ---> returns "Timothy"
-  #     if (length(x[which(x!="FALSE")])==1){
-  #       return(x[which(x!="FALSE")])
-  #     }
-  #
-  #   #
-  #     else {
-  #       tempResult <- x[which(x!="FALSE")]
-  #       tempResult <- x[which(nchar(x)==max(nchar(x)))][1] # if two duplicate results (e.g. "Timothy" and "Timothy"), then just return the 1st
-  #       return(tempResult)
-  #       # return(max(nchar(x))) #DEBUG
-  #     }
-  #     })
+  matchTemp3 <- lapply(matchTemp2, function(x) {
+
+    # if length of element is 0 then return FALSE
+      # if (length(x)==0) {
+      #  return("FALSE")
+      # }
+
+    # if all elements == "FALSE" then just return "FALSE"
+      if (length(x[which(x=="FALSE")])==length(x)) {
+        return("FALSE")
+      }
+
+    # if all elements except one == "FALSE" then return the 'non false' element
+    # e.g. c("FALSE", "FALSE", "Timothy", "FALSE") ---> returns "Timothy"
+      if (length(x[which(x!="FALSE")])==1){
+        return(x[which(x!="FALSE")])
+      }
+
+      else {
+        tempResult <- x[which(x!="FALSE")]
+        tempResult <- x[which(nchar(x)==max(nchar(x)))][1] # if two duplicate results (e.g. "Timothy" and "Timothy"), then just return the 1st
+        return(tempResult)
+        # return(max(nchar(x))) #DEBUG
+      }
+      })
 
   # matchTemp3 # what is this?
 
