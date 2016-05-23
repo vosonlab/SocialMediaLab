e98c0c6c41413a09a39d726e3a720ba971da0580
chainsawriot <chainsawtiney@gmail.com>
2016-05-23 17:55:23 +0800

    change .cred.R to cred.R in cred_empty.R

--- SocialMediaLab/tests/testthat/cred_empty.R
+++ SocialMediaLab/tests/testthat/cred_empty.R
@@ -1,5 +1,5 @@
-### please modify me and rename me to .cred.R
-### WARNING: don't add .cred.R to github repo
-### already in .gitignore
+### please modify me and rename me to cred.R
+### WARNING: don't add cred.R to github repo
+### cred.R already in .gitignore
 
 yt <- ""
