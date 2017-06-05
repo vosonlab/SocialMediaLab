c907d8690dc461ad41b3ba886f3472d4b535bc86
Tim Graham <timothy.graham3@uq.net.au>
2017-06-05 14:31:41 +1000

    Facebook bimodal network edges are now constructed using USER IDs rather than usernames. This solves the problem in the rare case that there are multiple users with the exact same first and last name. Usernames are provided as a vertex attribute called `username`

--- SocialMediaLab/R/CreateBimodalNetwork.facebook.R
+++ SocialMediaLab/R/CreateBimodalNetwork.facebook.R
@@ -29,11 +29,14 @@ function(x,writeToFile,removeTermsOrHashtags, ...)
   cat("\nCreating Facebook bimodal network...\n")
 
   # make a vector of all the unique actors in the network1
-  usersVec <- rep(c("User"),length(unique(dataCombinedUNIQUE$from)))
-  postsVec <- rep(c("Post"),length(unique(dataCombinedUNIQUE$to)))
-  usersAndPostsVec <- c(usersVec,postsVec)
-  actors <- data.frame(name=unique(factor(c(as.character(unique(dataCombinedUNIQUE$from)),
-  as.character(unique(dataCombinedUNIQUE$to))))),type=usersAndPostsVec)
+  # usersVec <- rep(c("User"),length(unique(dataCombinedUNIQUE$from)))
+  # postsVec <- rep(c("Post"),length(unique(dataCombinedUNIQUE$to)))
+  # usersAndPostsVec <- c(usersVec,postsVec)
+  # actors <- data.frame(name=unique(factor(c(as.character(unique(dataCombinedUNIQUE$from)),
+  # as.character(unique(dataCombinedUNIQUE$to))))),type=usersAndPostsVec)
+  actors <- data.frame(name=unique(c(
+                    unique(dataCombinedUNIQUE$from),unique(dataCombinedUNIQUE$to)
+                   )))
 
   # make a dataframe of the relations between actors
   # we need a dataframe here because igraph needs it AFAIK
@@ -47,7 +50,13 @@ function(x,writeToFile,removeTermsOrHashtags, ...)
 
   # construct a graph
   g <- graph.data.frame(relations, directed=TRUE, vertices=actors)
-
+  # we will add the vertex 'type' attribute after creating the network
+  V(g)$type <- "User" # default to user
+  V(g)$type[grep("_",V(g)$name)] <- "Post" # change the posts to post
+  # now we also need to add a username attribute for the users
+  V(g)$username <- dataCombinedUNIQUE$from_username[match(V(g)$name, dataCombinedUNIQUE$from)]
+  # we will just give the posts a username equal to their ID
+  V(g)$username[which(is.na(V(g)$username))] <- V(g)$name[which(is.na(V(g)$username))]
   # Make the node labels play nice with Gephi
   V(g)$label <- V(g)$name
 
